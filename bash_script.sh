9ac44fb89f1a71aa63e65f6a7cb297fc3fb7247f
Adhithyan <adhithyan@Adhithyans-MacBook-Air.local>
2026-01-15 13:56:49 +0530

    update the script message

--- bash_script.sh
+++ bash_script.sh
@@ -3,6 +3,6 @@
 while true
 do
 current_date=$(date)
-echo "$current_date:the script is up and running in docker!"
+echo "$current_date:the script is up and running in docker using Github Actions!"
 sleep 5
 done
